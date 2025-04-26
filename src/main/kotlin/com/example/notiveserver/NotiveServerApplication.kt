fcf2da133f087e9cd12d811c8c2c03dcab909afd
foura1201 <foura3207@gmail.com>
2025-04-26 21:57:39 +0900

    initial commit

---
+++ src/main/kotlin/com/example/notiveserver/NotiveServerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.notiveserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NotiveServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<NotiveServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/notiveserver/NotiveServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.notiveserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotiveServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
