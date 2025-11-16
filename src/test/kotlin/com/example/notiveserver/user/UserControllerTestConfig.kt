1a98cc41a9da0c731a04fffce073d19997c048c2
foura1201 <foura3207@gmail.com>
2025-11-16 13:29:02 +0900

    test: user controller 테스트 코드 추가

---
+++ src/test/kotlin/com/example/notiveserver/user/UserControllerTestConfig.kt
@@ -0,0 +1,33 @@
+package com.example.notiveserver.user
+
+import com.example.notiveserver.application.auth.AuthService
+import com.example.notiveserver.application.auth.TokenService
+import com.example.notiveserver.application.user.UserService
+import com.example.notiveserver.infrastructure.security.JwtTokenProvider
+import org.mockito.Mockito.mock
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext
+
+@TestConfiguration
+class UserControllerTestConfig {
+    @Bean
+    fun userService(): UserService =
+        mock(UserService::class.java)
+
+    @Bean
+    fun jwtTokenProvider(): JwtTokenProvider =
+        mock(JwtTokenProvider::class.java)
+
+    @Bean
+    fun authService(): AuthService =
+        mock(AuthService::class.java)
+
+    @Bean
+    fun tokenService(): TokenService =
+        mock(TokenService::class.java)
+
+    @Bean
+    fun jpaMappingContext(): JpaMetamodelMappingContext =
+        mock(JpaMetamodelMappingContext::class.java)
+}
